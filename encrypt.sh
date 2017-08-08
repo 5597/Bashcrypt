b8ef5ffc32a3a771a2d97c5c08d21cf97214f5cb
Rigved Tummala <tummalarr@gmail.com>
2017-08-08 14:26:31 -0400

    Add files via upload

---
+++ encrypt.sh
@@ -0,0 +1,45 @@
+echo "Welcome to file encrypter!"
+
+encryption="${1}"
+shift
+
+
+
+if [[ "${encryption}" = 'rot13' ]];
+then
+	for f in "${@}"; do
+		tr 'A-Za-z' 'N-ZA-Mn-za-m' < "${f}" > "${f}.rot13"
+	done
+
+	echo "File has been formatted to rot13!"
+fi
+
+
+if [[ "${encryption}" = 'base64' ]];
+then
+	for f in "${@}"; do
+		base64 < "${f}" > "${f}.base64"
+	done
+
+	echo "File has been formatted to base64!"
+fi
+
+
+if [[ "${encryption}" = 'sha1' ]];
+then
+	for f in "${@}"; do
+		sha1sum < "${f}" > "${f}.sha1"
+	done
+
+	echo "File has been formatted to sha1sum!"
+fi
+
+if [[ "${encryption}" = 'sha256' ]];
+then
+        for f in "${@}"; do
+                sha256sum < "${f}" > "${f}.sha256"
+        done
+
+        echo "File has been formatted to sha256sum!"
+fi
+
